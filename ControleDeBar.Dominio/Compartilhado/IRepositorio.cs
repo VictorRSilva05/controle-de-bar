434e53e56b29eb56481326d706dd82bea2cef308
Victor R. Silva <victorr.silva005@gmail.com>
2025-06-11 09:29:27 -0300

    Adicionado projeto de Domínnio

---
+++ ControleDeBar.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,12 @@
+﻿namespace ControleDeBar.Dominio.Compartilhado
+{
+    public interface IRepositorio<T> where T : EntidadeBase<T>
+    {
+        public void CadastrarRegistro(T novoRegistro);
+        public void EditarRegistro(T idRegistro, T registroEditado);
+        public void ExcluirRegistro(Guid idRegistro);
+        public List<T> SelecionarRegistros();
+        public T SelecionarRegistroPorId(Guid idRegistro);
+    }
+    
+}
