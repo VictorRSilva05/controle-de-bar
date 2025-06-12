477289f70aa2bc57b54f8a53f2d9348e1eb0f8a9
Victor R. Silva <victorr.silva005@gmail.com>
2025-06-12 16:02:34 -0300

    Página funcionado

---
+++ ControleDeBar.Dominio/ModuloGarcom/IRepositorioGarcom.cs
@@ -0,0 +1,7 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+using ControleDeBar.Dominio.ModuloGarcom;
+
+public interface IRepositorioGarcom : IRepositorio<Garcom>
+{
+}
+
---
+++ ControleDeBar.Infraestrutura.Arquivos/ModuloGarcom/RepositorioGarcomEmArquivo.cs
@@ -0,0 +1,33 @@
+﻿using ControleDeBar.Dominio.Compartilhado;
+using ControleDeBar.Dominio.ModuloGarcom;
+using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeBar.Infraestrutura.Arquivos.ModuloGarcom
+{
+    public class RepositorioGarcomEmArquivo : RepositorioBaseEmArquivo<Garcom>, IRepositorioGarcom
+    {
+        public RepositorioGarcomEmArquivo(ContextoDados contextoDados) : base(contextoDados)
+        {
+        }
+
+        protected override List<Garcom> ObterRegistros()
+        {
+            return contexto.Garcons;
+        }
+
+        void IRepositorio<Garcom>.EditarRegistro(Guid idRegistro, Garcom registroEditado)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRepositorio<Garcom>.ExcluirRegistro(Guid idRegistro)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
